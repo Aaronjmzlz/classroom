4ef0b00e94fd1bebc032d49701cc9e015b658b37
Aaronjmzlz <136303095+Aaronjmzlz@users.noreply.github.com>
2023-07-05 17:29:59 -0600

    Add files via upload
    Trabajo de Ciclos

---
+++ Ciclos.js
@@ -0,0 +1,12 @@
+function generarSecuencia() {
+  let numero = 1;
+  let incremento = 1;
+
+  while (numero <= 56) {
+    console.log(numero);
+    numero += incremento;
+    incremento++;
+  }
+}
+
+generarSecuencia();
